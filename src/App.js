f0eb4ea8a8fff93e3078ea497c978fc8784c4e66
Dipjyoti Kashyap <kaxyapdip@gmail.com>
2022-12-17 10:06:25 +0530

    update 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {ToastContainer} from "react-toastify";
+// import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/home/home";
+import React,{useEffect} from "react";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div className="App">
+            <Router>
+                <Home />
+            </Router>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </div >
+    );
 }
 
 export default App;
