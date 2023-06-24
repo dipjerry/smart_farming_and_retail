0318ba819ea1bc65f32c29a3f74b530ee8c801c5
Dipjyoti Kashyap <kaxyapdip@gmail.com>
2023-06-25 01:28:05 +0530

    tunnwl

--- src/apis/users.js
+++ src/apis/users.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const endpoint = `http://localhost:4007`;
+const endpoint = `https://5156-2409-40e6-b-365f-5957-6c25-1e79-1342.ngrok-free.app/`;
 
 let token = null;
 const config = {
